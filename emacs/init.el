3f16aceb499c053b24211ebf1900c93eb3b15815
Bruno Meneguele <bmeneguele@gmail.com>
2019-10-10 21:43:04 -0300

    emacs/init: changes to init packages

--- emacs/init.el
+++ emacs/init.el
@@ -67,9 +67,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yaml-mode company ac-slime helm-slime slime xcscope markdown-mode powerline use-package python-mode projectile magit helm flycheck evil-visual-mark-mode editorconfig))))
+    '(package-selected-packages
+         (quote
+             (rpm-spec-mode yaml-mode company ac-slime helm-slime slime xcscope markdown-mode powerline use-package python-mode projectile magit helm flycheck evil-visual-mark-mode editorconfig))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
