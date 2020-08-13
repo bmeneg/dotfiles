ea420e7dc85a3869176417def9caad1438cc56a6
Bruno Meneguele <bmeneguele@gmail.com>
2020-08-13 18:38:12 -0300

    emacs: update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -36,6 +36,9 @@
 (use-package editorconfig
     :ensure t
     :config (editorconfig-mode 1))
+(use-package xcscope
+	:ensure t
+	:init (cscope-setup))
 (use-package flycheck
     :ensure t
     :init (global-flycheck-mode))
@@ -47,8 +50,7 @@
 	:init (emmet-mode))
 
 ;; Show line number on the left
-(setq linum-format "%d ")
-(global-linum-mode 1)
+(global-display-line-numbers-mode)
 
 ;; Load theme
 (load-theme 'tango-dark)
@@ -58,7 +60,7 @@
 (set-frame-font "Bitstream Vera Sans Mono-9" nil t)
 
 ;; autofill automatically in all modes
-(setq-default auto-fill-funtion 'do-auto-fill)
+(setq-default auto-fill-function 'do-auto-fill)
 
 (add-hook 'sqml-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook 'emmet-mode)
@@ -73,9 +75,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-	'(package-selected-packages
-		 (quote
-			 (emmet-mode web-mode rpm-spec-mode yaml-mode company ac-slime helm-slime slime xcscope markdown-mode powerline use-package python-mode projectile magit helm flycheck evil-visual-mark-mode editorconfig))))
+ '(package-selected-packages
+   (quote
+    (emmet-mode web-mode rpm-spec-mode yaml-mode company ac-slime helm-slime slime xcscope markdown-mode powerline use-package python-mode projectile magit helm flycheck evil-visual-mark-mode editorconfig))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
