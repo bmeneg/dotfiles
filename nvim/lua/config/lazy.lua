60581d191d00e586f0a24309d4766664d58fc95c
Bruno Meneguele <bmeneg@heredoc.io>
2025-12-09 11:06:55 -0300

    nvim/lazy: limit concurrent processes
    When using unlimited number of concurrent tasks to update/fetch/... with
    Lazy I was facing some issues during git-fetch authentication, which might
    be a problem/limitation on the gpg-agent trying to deal with too many
    requests for SSH key authentication or with git-fetch itself. Regardless the
    culprit, limiting it to a number like 8 got rid of the issue.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,4 +23,6 @@ require("lazy").setup({
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
+	-- too many concurrent processes were causing issues with `git fetch`
+	concurrency = 8,
 })
